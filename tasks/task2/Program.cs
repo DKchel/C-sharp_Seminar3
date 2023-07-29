34cc6af7fd3adf16726490e79cb1f5777e3e69f8
KDA <kuda74ru@gmail.ru>
2023-07-29 12:33:24 +0500

    add task2

---
+++ tasks/task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+
+Console.WriteLine("Введите номер четверти (1 - 4)");
+int quater = Convert.ToInt32(Console.ReadLine());
+
+
+if (quater == 1)
+{
+    Console.WriteLine("(Координаты точки: X > 0 и Y > 0");
+}
+
+else if (quater == 2)
+{
+    Console.WriteLine("(Координаты точки: X < 0 и Y > 0");
+}
+
+else if (quater == 3)
+{
+    Console.WriteLine("(Координаты точки: X < 0 и Y < 0");
+}
+
+else if (quater == 4)
+{
+    Console.WriteLine("(Координаты точки: X > 0 и Y < 0");
+}
